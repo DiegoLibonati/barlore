4800cc89107392d1da5f852005c5b13a4bd9104c
Die <dlibonati@leafnoise.io>
2026-05-01 20:06:31 -0300

    feat: better response type

--- src/services/cocktailService.ts
+++ src/services/cocktailService.ts
@@ -1,28 +1,33 @@
-import type { Cocktail } from "@/types/app";
+import type {
+  ResponseDirect,
+  ResponseDrinkNull,
+  ResponseDrinks,
+  ResponseDrinksNull,
+} from "@/types/responses";
 
 const cocktailService = {
-  getAll: async (search: string): Promise<Cocktail[] | string> => {
+  getAll: async (search: string): Promise<ResponseDirect<ResponseDrinks["drinks"]>> => {
     const response = await fetch(`/api/json/v1/1/search.php?f=${search}`, {
       method: "GET",
     });
 
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    const data = (await response.json()) as { drinks: Cocktail[] | string };
-    const cocktails: Cocktail[] | string = data.drinks;
+    const data = (await response.json()) as ResponseDirect<ResponseDrinks>;
+    const cocktails: ResponseDrinks["drinks"] = data.drinks;
 
     return cocktails;
   },
 
-  getById: async (id: string): Promise<Cocktail | null | string> => {
+  getById: async (id: string): Promise<ResponseDirect<ResponseDrinkNull>> => {
     const response = await fetch(`/api/json/v1/1/lookup.php?i=${id}`, {
       method: "GET",
     });
 
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    const data = (await response.json()) as { drinks: Cocktail[] | string | null };
-    const cocktail: Cocktail[] | string | null = data.drinks;
+    const data = (await response.json()) as ResponseDirect<ResponseDrinksNull>;
+    const cocktail: ResponseDrinksNull["drinks"] = data.drinks;
 
     if (!cocktail || typeof cocktail === "string") return cocktail;
 
--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,3 +1,5 @@
+import type { Cocktail } from "@/types/app";
+
 export interface DefaultResponse {
   code: string;
   message: string;
@@ -6,3 +8,15 @@ export interface DefaultResponse {
 export interface ResponseWithData<T> extends DefaultResponse {
   data: T;
 }
+
+export type ResponseDirect<T> = T;
+
+export interface ResponseDrinks {
+  drinks: Cocktail[] | string;
+}
+
+export interface ResponseDrinksNull {
+  drinks: Cocktail[] | string | null;
+}
+
+export type ResponseDrinkNull = Cocktail | string | null;

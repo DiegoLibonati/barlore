f876381131a7d108b0de32991b4dc27e699df992
Die <dlibonati@leafnoise.io>
2026-06-03 15:30:00 -0300

    fix: migrate react-router-dom to react-router

--- src/pages/CocktailDetailPage/CocktailDetailPage.tsx
+++ src/pages/CocktailDetailPage/CocktailDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router";
 
 import type { JSX } from "react";
 import type { Cocktail } from "@/types/app";
